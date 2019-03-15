8af8ebce02d700bfde11bed94f07823af7b98523
Ashkan Rafiee <35492414+AshkanRafiee@users.noreply.github.com>
2019-03-15 19:35:00 +0330

    Add files via upload

---
+++ Phonebook.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Phonebook</title>
+</head>
+<body>
+
+
+
+</body>
+</html>
+
---
+++ Phonebook.sql
@@ -0,0 +1,11 @@
+CREATE TABLE phonebook 
+(id int(3) not null AUTO_INCREMENT Primary key , Name varchar(100),
+LastName varchar(100) , Phone varchar(100) );
+
+
+Insert Into phonebook 
+(Name , LastName , Phone) 
+Value 
+(Ashkan,Rafiee,09366473524),(Ashi,Mashi,09352181218);
+
+
